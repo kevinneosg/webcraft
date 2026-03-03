d00737e2ba81e2c3fe756f7d3275b3559a38fd5d
kevinneosg <kevin.neo.jh@gmail.com>
2026-03-04 03:49:51 +0800

    Fix RootLayout types and add payload migrate to build script

--- src/app/(payload)/admin/[[...payload]]/layout.tsx
+++ src/app/(payload)/admin/[[...payload]]/layout.tsx
@@ -1,22 +1,11 @@
 import { RootLayout } from '@payloadcms/next/layouts'
+import { serverFunction } from '@payloadcms/next/views'
 import { importMap } from '../importMap'
 import config from '../../../../payload.config'
 import './custom.css'
 
 type Props = {
   children: React.ReactNode
-}
-
-const serverFunction = async ({ name, args }: { name: string; args: Record<string, unknown> }) => {
-  'use server'
-  const mod = await import(/* @vite-ignore */ name)
-  if (!mod) {
-    throw new Error(`Module not found: ${name}`)
-  }
-  if (typeof mod.default !== 'function') {
-    throw new Error(`Default export of ${name} is not a function`)
-  }
-  return mod.default(args)
 }
 
 const Layout = ({ children }: Props) => (
